2c1fced7cc79bce43223b51f3deceeeffbd2d237
ASTHARUNPRANAV <a7.s7.pranav.0.nothing@gmail.com>
2026-06-09 16:10:32 +0530

    Create

---
+++ problem-1781001629254/version/python/problem-1781001629254_20260609_161029.py
@@ -0,0 +1,11 @@
+# Last updated: 6/9/2026, 4:10:29 PM
+1class Solution:
+2    def isPalindrome(self, x: int) -> bool:
+3        if x < 0:
+4            return False
+5        reverse = 0
+6        copy = x
+7        while x > 0:
+8            reverse = (reverse * 10)+(x%10)
+9            x//=10
+10        return reverse == copy
